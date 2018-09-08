54e8f3d14b3cbee94de0e8a39e73bd5abfd9d143
Scott Kuhl <scott@kuhl.ws>
2018-09-07 21:06:36 -0500

    Initial project.

--- DeckOfCards/Program.cs
+++ DeckOfCards/Program.cs
@@ -2,11 +2,20 @@
 
 namespace DeckOfCards
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            // Create a deck cards.
+            Deck deck = new Deck();
+
+            // Draw a card.
+            var card = deck.Draw();
+
+            // Show the value.
+            Console.WriteLine(card.GetFullName());
+
+            Console.ReadLine();
         }
     }
 }

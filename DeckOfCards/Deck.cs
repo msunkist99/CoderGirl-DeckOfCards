284809f55974677f93f5018425065d76c6d1ee25
msunkist99 <msunkist99@gmail.com>
2019-02-16 14:30:33 -0600

    2019/02/16 finish project

--- DeckOfCards/Deck.cs
+++ DeckOfCards/Deck.cs
@@ -12,14 +12,54 @@ namespace DeckOfCards
             int cardCount = 0;
             _random = new Random();
 
+            string suit;
+
             // TODO: Load the card deck with a standard deck of 52 cards.  Use loops, do not hard code each one.
             for (int i = 0; i < 4; i++)
             {
+                if (i == 0)
+                {
+                    suit = "Hearts";
+                }
+                else if (i == 1)
+                {
+                    suit = "Diamonds";
+                }
+                else if (i == 2)
+                {
+                    suit = "Clubs";
+                }
+                else
+                {
+                    suit = "Spades";
+                }
+
+
                 for (int j = 1; j < 14; j++)
                 {
                     Card card = new Card();
-                    card.Suit = i.ToString();
-                    card.FaceValue = j.ToString();
+                    card.Suit = suit;
+
+                    if (j == 1)
+                    {
+                        card.FaceValue = "Ace";
+                    }
+                    else if (j == 11)
+                    {
+                        card.FaceValue = "Jack";
+                    }
+                    else if (j == 12)
+                    {
+                        card.FaceValue = "Queen";
+                    }
+                    else if (j == 13)
+                    {
+                        card.FaceValue = "King";
+                    }
+                    else
+                    {
+                        card.FaceValue = j.ToString();
+                    }
 
                     deckOfCards[cardCount] = card;
                     cardCount++;
@@ -33,8 +73,10 @@ namespace DeckOfCards
 
         public Card Draw()
         {
+            int random = _random.Next(0, 53);
+                        
             // TODO: Return a random card from the deck.
-            return null;
+            return Cards[random];
         }
     }
 }
